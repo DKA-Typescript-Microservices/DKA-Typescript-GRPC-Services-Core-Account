397c9e82b655fb2a4746da74e26ecaa0f461b24a
Yovangga Anandhika <dka.tech.dev@gmail.com>
2025-06-04 00:40:04 +0800

    adding not implemented method grpc config

--- src/config/const/proto.array.config.ts
+++ src/config/const/proto.array.config.ts
@@ -3,5 +3,5 @@ import * as path from 'node:path';
 const pathModel = path.join(__dirname, './../../model/proto');
 export const ProtoArrayConfig = {
   package: ['account', 'credential', 'info'],
-  protoPath: [path.join(pathModel, './account/account.grpc.proto')],
+  protoPath: [path.join(pathModel, './account/account.grpc.proto'), path.join(pathModel, './account/credential/account.credential.grpc.proto')],
 };
